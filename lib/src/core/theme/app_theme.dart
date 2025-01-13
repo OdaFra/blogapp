b7680503bcd6ab15ae66514f411a39a83622d5c0
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-13 18:18:20 -0300

    feat: Proceso para generacion de temas para la app

---
+++ lib/src/core/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:blogapp/src/core/theme/color_theme.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static _border([Color color = ColorTheme.borderColor]) => OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 3,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      );
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: ColorTheme.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(ColorTheme.gradient2),
+      
+    ),
+  );
+}
---
+++ lib/src/core/theme/color_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorTheme {
+  static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
+  static const Color gradient1 = Color(0xFF006DA4);
+  static const Color gradient2 = Color(0xFF128BC7);
+  static const Color gradient3 = Color(0xFF8AAEED);
+  static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
+  static const Color whiteColor = Colors.white;
+  static const Color greyColor = Colors.grey;
+  static const Color errorColor = Colors.redAccent;
+  static const Color transparentColor = Colors.transparent;
+}
