9bece12f288ac04000946f0c40147d548bf0247a
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-17 09:19:45 -0300

    feat: Generamos los archivos de caso de uso para registro

---
+++ lib/src/core/usecase/usecase.dart
@@ -0,0 +1,9 @@
+import 'package:fpdart/fpdart.dart';
+
+import '../error/error.dart';
+
+abstract interface class Usecase<SuccessType, Params> {
+  Future<Either<Failure, SuccessType>> call(
+    Params params,
+  );
+}
---
+++ lib/src/features/auth/domain/usescases/user_sign_up.dart
@@ -0,0 +1,30 @@
+import 'package:blogapp/src/core/error/failure.dart';
+import 'package:blogapp/src/core/usecase/usecase.dart';
+import 'package:blogapp/src/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  const UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailAndPassword(
+      username: params.username,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String username;
+  final String email;
+  final String password;
+
+  UserSignUpParams({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
