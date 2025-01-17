6d42b13719539d912afd55c1925eec4e3ae87cda
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-17 18:23:59 -0300

    feat: Agregamos VoidCallback como requerido en el AuthButton

--- lib/src/features/auth/presentation/pages/login_page.dart
+++ lib/src/features/auth/presentation/pages/login_page.dart
@@ -54,8 +54,9 @@ class _LoginPageState extends State<LoginPage> {
               isObscureText: true,
             ),
             const SizedBox(height: 30),
-            const AuthButton(
+            AuthButton(
               buttonText: 'Sign In',
+              onPressed: () {},
             ),
             const SizedBox(height: 20),
             GestureDetector(
--- lib/src/features/auth/presentation/widgets/auth/auth_button.dart
+++ lib/src/features/auth/presentation/widgets/auth/auth_button.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 
 class AuthButton extends StatelessWidget {
   final String buttonText;
-  const AuthButton({
-    super.key,
-    required this.buttonText,
-  });
+  final VoidCallback onPressed;
+  const AuthButton(
+      {super.key, required this.buttonText, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +22,7 @@ class AuthButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(15),
       ),
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           fixedSize: const Size(395, 55),
           backgroundColor: ColorTheme.transparentColor,
