f6f5001f7fe9cd88380d7a2d04d7e7df88800fc3
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-17 18:25:23 -0300

    feat: Agregamos el bloc de Auth en la rama principal del widget para la inicializacion del servicio a supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,39 @@
 import 'package:blogapp/src/core/secrets/app_secrets.dart';
+import 'package:blogapp/src/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blogapp/src/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:blogapp/src/features/auth/domain/usescases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:blogapp/src/core/theme/app_theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'src/features/auth/presentation/bloc/auth_bloc.dart';
 import 'src/features/auth/presentation/pages/pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppSecrets.loadEnv();
 
-  await Supabase.initialize(
+  final supabase = await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
     debug: true,
   );
 
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => AuthBloc(
+          userSignUp: UserSignUp(
+            authRepository: AuthRepositoryImpl(
+                remoteDataSource:
+                    AuthRemoteDataSourceImpl(client: supabase.client)),
+          ),
+        ),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
