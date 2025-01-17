8fcdc1d3d04d67a9c45a4dcc39715e54245c8ddc
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-01-17 18:24:40 -0300

    feat: Generamos la logica para el registro considerando el AuthSingUp previamente gestionado

--- lib/src/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/src/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,35 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:blogapp/src/features/auth/domain/usescases/user_sign_up.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserSignUp _userSignUp;
+
+  AuthBloc({required UserSignUp userSignUp})
+      : _userSignUp = userSignUp,
+        super(
+          AuthInitial(),
+        ) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(
+        UserSignUpParams(
+          username: event.username,
+          email: event.email,
+          password: event.password,
+        ),
+      );
+
+      res.fold(
+        (failure) => emit(
+          AuthFailure(failure.message),
+        ),
+        (uid) => emit(
+          AuthSuccess(uid),
+        ),
+      );
     });
   }
 }
