a32c7cb73406efea019fda285eca8ae24d1a42c0
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-03-25 18:44:09 -0300

    refactor: Se paramos importaciones de la funcion principal para la injeccion de dependecias.

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:blogapp/src/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:blogapp/src/core/network/connection_check.dart';
 import 'package:blogapp/src/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blogapp/src/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:blogapp/src/features/auth/domain/repository/auth_repository.dart';
@@ -6,6 +7,7 @@ import 'package:blogapp/src/features/auth/domain/usescases/current_user.dart';
 import 'package:blogapp/src/features/auth/domain/usescases/user_login.dart';
 import 'package:blogapp/src/features/auth/domain/usescases/user_sign_up.dart';
 import 'package:blogapp/src/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blogapp/src/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:blogapp/src/features/blog/data/datasources/blog_remote_data_sources.dart';
 import 'package:blogapp/src/features/blog/data/repository/blog_repositry_impl.dart';
 import 'package:blogapp/src/features/blog/domain/repository/blog_repository.dart';
@@ -13,91 +15,11 @@ import 'package:blogapp/src/features/blog/domain/usescases/get_all_blogs.dart';
 import 'package:blogapp/src/features/blog/domain/usescases/upload_blog.dart';
 import 'package:blogapp/src/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'src/core/secrets/secrets.dart';
 
-final serviceLocator = GetIt.instance;
-
-Future<void> initDependecies() async {
-  _initAuth();
-  _initBlog();
-
-  await AppSecrets.loadEnv();
-
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-    debug: true,
-  );
-
-  serviceLocator.registerLazySingleton(() => supabase.client);
-
-  //core
-  serviceLocator.registerLazySingleton(() => AppUserCubit());
-}
-
-void _initAuth() {
-  //==>DataSources
-  serviceLocator
-    ..registerFactory<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl(
-        supabaseClient: serviceLocator(),
-      ),
-    )
-    //==>Repositories
-    ..registerFactory<AuthRepository>(
-      () => AuthRepositoryImpl(
-        remoteDataSource: serviceLocator(),
-      ),
-    )
-    //==>UseCases
-    ..registerFactory(
-      () => UserSignUpUseCase(
-        authRepository: serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => UserLoginUseCase(
-        authRepository: serviceLocator(),
-      ),
-    )
-    ..registerFactory(() => CurrentUserUseCase(
-          authRepository: serviceLocator(),
-        ))
-    //==>Blocs
-    ..registerLazySingleton(
-      () => AuthBloc(
-        userSignUp: serviceLocator(),
-        userLogin: serviceLocator(),
-        currentUserUseCase: serviceLocator(),
-        appUserCubit: serviceLocator(),
-      ),
-    );
-}
-
-void _initBlog() {
-  //==>DataSources
-  serviceLocator
-    ..registerFactory<BlogRemoteDataSource>(() => BlogRemoteDataSourceImpl(
-          supabaseClient: serviceLocator(),
-        ))
-    //==>Repositories
-    ..registerFactory<BlogRepository>(() => BlogRepositryImpl(
-          remoteDataSource: serviceLocator(),
-        ))
-    //==>UseCases
-    ..registerFactory(() => UploadBlogUseCase(
-          blogRepository: serviceLocator(),
-        ))
-    ..registerFactory(() => GetAllBlogsUseCase(
-          blogRepository: serviceLocator(),
-        ))
-    //==>Blocs
-    ..registerLazySingleton(
-      () => BlogBloc(
-        uploadBlogUseCase: serviceLocator(),
-        getAllBlogsUseCase: serviceLocator(),
-      ),
-    );
-}
+part 'init_dependencies.main.dart';
---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,110 @@
+part of 'init_dependencies.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependecies() async {
+  _initAuth();
+  _initBlog();
+
+  await AppSecrets.loadEnv();
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+    debug: true,
+  );
+
+// Initialize Supabase
+  serviceLocator.registerLazySingleton(() => supabase.client);
+
+// Initialize Hive
+  Hive.defaultDirectory = (await getApplicationCacheDirectory()).path;
+  serviceLocator.registerLazySingleton(() => Hive.box(name: 'blogs'));
+
+  // Internet Connection
+  serviceLocator.registerFactory(
+    () => InternetConnection(),
+  );
+
+  //core
+  serviceLocator.registerLazySingleton(
+    () => AppUserCubit(),
+  );
+
+  //Check Connection
+  serviceLocator.registerFactory<ConnectionCheck>(
+    () => ConnectionCheckImpl(
+      internetConnection: serviceLocator(),
+    ),
+  );
+}
+
+void _initAuth() {
+  //==>DataSources
+  serviceLocator
+    ..registerFactory<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(
+        supabaseClient: serviceLocator(),
+      ),
+    )
+    //==>Repositories
+    ..registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(
+        remoteDataSource: serviceLocator(),
+        connectionCheck: serviceLocator(),
+      ),
+    )
+    //==>UseCases
+    ..registerFactory(
+      () => UserSignUpUseCase(
+        authRepository: serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => UserLoginUseCase(
+        authRepository: serviceLocator(),
+      ),
+    )
+    ..registerFactory(() => CurrentUserUseCase(
+          authRepository: serviceLocator(),
+        ))
+    //==>Blocs
+    ..registerLazySingleton(
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        userLogin: serviceLocator(),
+        currentUserUseCase: serviceLocator(),
+        appUserCubit: serviceLocator(),
+      ),
+    );
+}
+
+void _initBlog() {
+  //==>DataSources
+  serviceLocator
+    ..registerFactory<BlogRemoteDataSource>(() => BlogRemoteDataSourceImpl(
+          supabaseClient: serviceLocator(),
+        ))
+    ..registerFactory<BlogLocalDataSource>(
+        () => BlogLocalDataSourceImpl(serviceLocator()))
+    //==>Repositories
+    ..registerFactory<BlogRepository>(() => BlogRepositryImpl(
+          remoteDataSource: serviceLocator(),
+          localDataSource: serviceLocator(),
+          connectionCheck: serviceLocator(),
+        ))
+    //==>UseCases
+    ..registerFactory(() => UploadBlogUseCase(
+          blogRepository: serviceLocator(),
+        ))
+    ..registerFactory(() => GetAllBlogsUseCase(
+          blogRepository: serviceLocator(),
+        ))
+    //==>Blocs
+    ..registerLazySingleton(
+      () => BlogBloc(
+        uploadBlogUseCase: serviceLocator(),
+        getAllBlogsUseCase: serviceLocator(),
+      ),
+    );
+}
