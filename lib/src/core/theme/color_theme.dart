4c813807988420c2bd35a259a9065eb603d34e1d
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-03-25 18:37:35 -0300

    refactor: Actualizamos los colores para la app y aplicamos un validador de format_date para la fecha obtenida

--- lib/src/core/theme/color_theme.dart
+++ lib/src/core/theme/color_theme.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ColorTheme {
-  static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
-  static const Color gradient1 = Color(0xFF006DA4);
-  static const Color gradient2 = Color(0xFF128BC7);
-  static const Color gradient3 = Color(0xFF8AAEED);
-  static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
+  static const Color backgroundColor =
+      Color(0xFF0A192F); // Azul marino profundo
+  static const Color gradient1 = Color(0xFF5E60CE); // Azul violeta vibrante
+  static const Color gradient2 = Color(0xFF4EA8DE); // Azul cielo llamativo
+  static const Color gradient3 = Color(0xFF48BFE3); // Azul turquesa fresco
+  static const Color borderColor =
+      Color(0xFF4C5B7B); // Azul grisáceo más visible
   static const Color whiteColor = Colors.white;
-  static const Color greyColor = Colors.grey;
-  static Color errorColor = Colors.red.shade300;
+  static const Color greyColor = Color(0xFFC0C0C0); // Gris claro suave
+  static Color errorColor =
+      const Color(0xFFE63946); // Rojo más fuerte y visible
   static const Color transparentColor = Colors.transparent;
 }
