03d69c7707b667d6aab32c33c6137f5d803bab59
Oscar Ramirez <oscar.ramirez@millicom.com>
2025-05-11 16:03:45 -0300

    config: Agregamos archivos sql con respectos a las tablas de blog y profiles

---
+++ supabase/blog.sql
@@ -0,0 +1,40 @@
+-- Create a table for public blogs
+create table blogs (
+  id uuid not null primary key,
+  updated_at timestamp with time zone,
+  poster_id uuid not null,
+  title text not null,
+  content text not null,
+  image_url text,
+  topics text array,
+  foreign key (poster_id) references public.profiles(id)
+);
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/database/postgres/row-level-security for more details.
+alter table profiles
+  enable row level security;
+
+create policy "Public blogs are viewable by everyone." on blogs
+  for select using (true);
+
+create policy "Users can insert their own blogs." on blogs
+  for insert with check ((select auth.uid()) = id);
+
+create policy "Users can update own blogs." on blogs
+  for update using ((select auth.uid()) = id);
+
+
+-- Set up Storage!
+insert into storage.buckets (id, name)
+  values ('blog_images', 'blog_images');
+
+-- Set up access controls for storage.
+-- See https://supabase.com/docs/guides/storage/security/access-control#policy-examples for more details.
+create policy "Avatar images are publicly accessible." on storage.objects
+  for select using (bucket_id = 'blog_images');
+
+create policy "Anyone can upload an avatar." on storage.objects
+  for insert with check (bucket_id = 'blog_images');
+
+create policy "Anyone can update their own avatar." on storage.objects
+  for update using ((select auth.uid()) = owner) with check (bucket_id = 'blog_images');
